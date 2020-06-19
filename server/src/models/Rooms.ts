6ac3a4bafe070e21e69ea906395d35fe4ef9f636
Maciej Kwietniewski <maciek.kwiet36@gmail.com>
2020-06-19 16:41:26 +0200

    moved logic to class

--- server/src/models/Rooms.ts
+++ server/src/models/Rooms.ts
@@ -1,18 +1,37 @@
 import { Room } from './Room';
+import { User } from './User';
 
 class Rooms {
   public rooms: Room[];
-
   constructor(rooms: Room[]) {
     if (!this.rooms) this.rooms = [];
   }
 
-  doesRoomExist(roomId: number): boolean {
+  doesRoomExist(roomId: number, newUser: User): boolean {
     let roomExist: boolean = false;
     const param = this.rooms.map((existingRoom: Room) => {
       existingRoom.roomId == roomId ? (roomExist = true) : '';
     });
-    return roomExist;
+    let messageStatus = roomExist ? this.roomExist(roomId, newUser) : this.roomNoExist(roomId, newUser);
+    return messageStatus;
+  }
+
+  roomExist(roomId: number, newUser: User) {
+    let messageStatus: boolean;
+    this.rooms.map((room: Room) => {
+      if (room.roomId == roomId && !room.doesUserExist(newUser)) {
+        room.users.push(newUser);
+        messageStatus = true;
+      } else messageStatus = false;
+    });
+    return messageStatus;
+  }
+
+  roomNoExist(roomId: number, newUser: User): boolean {
+    const newRoom = new Room(roomId, null);
+    newRoom.users.push(newUser);
+    this.rooms.push(newRoom);
+    return true;
   }
 }
 export { Rooms };
--- server/src/services/onUserJoin.ts
+++ server/src/services/onUserJoin.ts
@@ -1,5 +1,4 @@
 import * as socketio from 'socket.io';
-import { Room } from '../models/Room';
 import { User } from '../models/User';
 import { Rooms } from 'models/Rooms';
 
@@ -15,23 +14,10 @@ const onUserJoin = (io: socketio.Server, rooms: Rooms, socket: socketio.Socket) 
   isAdmin,
 }: UserJoinPayload) => {
   const newUser = new User(name, null, isAdmin);
-  let message: string = ``;
-  let messageStatus: boolean = true;
-  if (rooms.doesRoomExist(roomId)) {
-    rooms.rooms.map((room: Room) => {
-      if (room.roomId == roomId && !room.doesUserExist(newUser)) {
-        room.users.push(newUser);
-      } else messageStatus = false;
-    });
-  } else {
-    const newRoom = new Room(roomId, null);
-    newRoom.users.push(newUser);
-    rooms.rooms.push(newRoom);
-  }
-
-  messageStatus
-    ? (message = `${name} has joined the room: ${roomId.toString()}`)
-    : (message = `User ${name} is already in room: ${roomId.toString()}`);
+  let messageStatus: Boolean = rooms.doesRoomExist(roomId, newUser);
+  let message: string = messageStatus
+    ? `${name} has joined the room: ${roomId.toString()}`
+    : `User ${name} is already in room: ${roomId.toString()}`;
   console.log(message);
   socket.join(roomId.toString());
   io.to(roomId.toString()).emit('FEED', message);
