bf13cfbef1d02f67c135b76af5c7bef43d510c1f
Maciej Kwietniewski <maciek.kwiet36@gmail.com>
2020-06-23 19:47:46 +0200

    dataSession first version

--- server/src/router/dataSession.ts
+++ server/src/router/dataSession.ts
@@ -1,8 +1,11 @@
 import * as express from 'express';
+import { rooms } from 'models/Rooms';
+
 const router = express.Router();
 
 router.get('/', async (req: express.Request, res: express.Response) => {
-  //xxx
+  const room = rooms.getRoom(req.body);
+  res.json({ room: room });
 });
 
 module.exports = router;
