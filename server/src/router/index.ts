41a0e7c2458ec793faa940d6b6dfe697bffa5d3b
Maciej Kwietniewski <maciek.kwiet36@gmail.com>
2020-06-23 18:45:15 +0200

    mini fix

--- server/src/router/index.ts
+++ server/src/router/index.ts
@@ -1,7 +1,10 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 
 const newSession = require('./newSession');
+const dataSession = require('./dataSession');
 
 router.use('/newSession', newSession);
+router.use('/newSession', dataSession);
 
 module.exports = router;
