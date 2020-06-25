873833cdf1d2b6dd52e16f12195c9b13fe9303d2
unknown <kaacper.janusz@gmail.com>
2020-06-25 18:21:27 +0200

    to test

--- server/src/models/Rooms.ts
+++ server/src/models/Rooms.ts
@@ -5,14 +5,28 @@ class Rooms {
 
   getRoom(roomId: number): Room {
     //Do wywalenia
-    const room = this.rooms.find(({ id }) => id === roomId) ?? rooms.createRoom(roomId);
+    const room = this.rooms.find(({ id }) => id === roomId) ?? rooms.createRoom();
     return room;
   }
 
-  private createRoom(roomId: number): Room {
-    const room = new Room(roomId);
-    this.rooms.push(room);
-    return room;
+  public createRoom(): Room {
+    let uniqueId = this.createRandomId();
+
+    if (this.checkId(uniqueId)) {
+      const room = new Room(uniqueId);
+      this.rooms.push(room);
+      return room;
+    }
+  }
+  private createRandomId(): number {
+    return Math.floor(Math.random() * 10000);
+  }
+  private checkId(id): boolean {
+    let status: boolean = false;
+    this.rooms.map(room => {
+      room.id == id ? (status = true) : null;
+    });
+    return status;
   }
 }
 
