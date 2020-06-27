bddf5b21dbebf887f8a317ce3b16447b329c702f
Tom-Bul <t.bulat99@gmail.com>
2020-06-27 23:07:11 +0200

    added logo.png to src/assets/images

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Box } from '@material-ui/core';
 import HeaderStyles from './HeaderStyles';
+import Logo from './../../Assets/Images/logo.png';
 
 const Header = () => {
-  const imgSrc = 'https://files.slack.com/files-pri/TCB921BEE-F015MT9B5N3/logo.png';
   const classes = HeaderStyles();
 
   return (
     <Box className={classes.box}>
-      <img className={classes.img} src={imgSrc} alt="logo"></img>
+      <img className={classes.img} src={Logo} alt="logo"></img>
     </Box>
   );
 };
