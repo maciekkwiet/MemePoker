0f5a9a6c5ccaef09cecf3adbbe844b01dab9ac60
Maciej Kwietniewski <maciek.kwiet36@gmail.com>
2020-06-29 23:22:53 +0200

    clearingVotes_showingVotes

---
+++ server/src/controllers/onClearVotes.ts
@@ -0,0 +1,15 @@
+import * as socketio from 'socket.io';
+import { rooms } from '@models/Rooms';
+
+const onClearVotes = (io: socketio.Server, socket: socketio.Socket) => (roomId: number) => {
+  const room = rooms.getRoom(roomId);
+
+  if (typeof room === 'string') return console.error(room);
+
+  room.clearVotes();
+
+  const message = `All votes have been reset`;
+  io.to(roomId.toString()).emit('FEED', message);
+};
+
+export { onClearVotes };
