c7b8f49294cead0f3205ccec291422cc7b8a7501
KlaraGajaszek <klara.gajaszek@student.po.edu.pl>
2020-07-07 17:33:47 +0200

    testnewroom

--- client/src/Components/Results/Results.jsx
+++ client/src/Components/Results/Results.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Paper, Typography, Box } from '@material-ui/core';
+import { useEmit } from 'socketio-hooks';
+import { useParams } from 'react-router-dom';
 
 import UserVotes from 'Components/UserVotes';
 import VoteBtn from 'Components/VoteButton';
 import ResultsStyles from './ResultsStyles';
+import { useUserContext } from 'Contexts/UserContext';
 
 const Results = () => {
   const classes = ResultsStyles();
+  const { roomId } = useParams();
+  const { userRooms } = useUserContext();
+  const sendVotes = useEmit('SHOW_VOTES');
+
+  const onClickHandler = () => {
+    sendVotes({ roomId });
+    console.log({ userRooms });
+    console.log({ roomId });
+  };
 
   return (
     <Paper className={classes.root}>
@@ -15,7 +27,7 @@ const Results = () => {
       </Typography>
       <UserVotes />
       <Box className={classes.btnWrap}>
-        <VoteBtn content="Show votes" />
+        <VoteBtn content="Show votes" btnFunction={onClickHandler} />
         <VoteBtn content="Clear votes" />
       </Box>
     </Paper>
