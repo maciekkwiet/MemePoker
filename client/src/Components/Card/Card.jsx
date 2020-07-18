e39354d3ab2bc79ebd216dd265c364a6b5d76053
Maciej Kwietniewski <maciek.kwiet36@gmail.com>
2020-07-18 15:25:28 +0200

    first version

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useEmit } from 'socketio-hooks';
 import { Card, CardActionArea, CardMedia, CardContent, Typography } from '@material-ui/core';
@@ -12,12 +12,18 @@ const MemeCard = ({ value, img }) => {
   const { roomId } = useParams();
   const classes = cardStyles();
 
+  const [selected, setSelected] = useState(false);
+
   const name = getUserName(roomId);
 
-  const onClickHandler = () => sendEstimation({ name, value, roomId });
+  const onClickHandler = e => {
+    sendEstimation({ name, value, roomId });
+    setSelected(!selected);
+    console.log(selected);
+  };
 
   return (
-    <Card className={classes.main} variant="outlined">
+    <Card className={selected ? classes.maintwo : classes.main} variant="outlined">
       <CardActionArea onClick={onClickHandler}>
         <CardMedia className={classes.media} component="img" alt={value} src={img} height="50" />
         <CardContent className={classes.content}>
