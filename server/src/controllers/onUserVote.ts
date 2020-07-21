75730496cc2367a3df562724a6cbd330e10e9362
CaptainObjective <marcinwos18@gmail.com>
2020-07-21 19:41:52 +0200

    added task analyzis

--- server/src/controllers/onUserVote.ts
+++ server/src/controllers/onUserVote.ts
@@ -8,6 +8,8 @@ const onUserVote: EventHandler<UserVotePayload> = ({ io }, { value, room, user }
   if (room.hasEveryoneVoted()) {
     room.archiveTask();
 
+    room.getTask().setResults(room.getVotes());
+    room.getTask().analyzeResults();
     message = `Everyone in room ${room.id} voted, votes: ${JSON.stringify(room.getVotes())}`;
 
     io.to(room.id).emit('ROOM_VOTES', { task: room.getTask(), votes: room.getVotes() });
