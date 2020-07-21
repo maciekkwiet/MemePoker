336dda2167c6a03a1f5c9bc5617519da195b6dff
KlaraGajaszek <klara.gajaszek@student.po.edu.pl>
2020-07-21 23:51:07 +0200

    first version

--- client/src/hooks/useBackend.js
+++ client/src/hooks/useBackend.js
@@ -4,17 +4,15 @@ import { useUserContext } from 'Contexts/UserContext';
 /**
  * Used to talk with backend, applies token to each request emitted
  * @param {string} eventName
- * @returns {function}
+ * @returns {function} function to any request with token attache
  */
 const useBackend = eventName => {
   const emitter = useEmit(eventName);
   const { token } = useUserContext();
 
-  return (payload, callback) => {
-    const request = payload ?? {};
-    request.token = token;
-    emitter(request, callback);
+  return (payload = {}, callback) => {
+    payload.token = token;
+    emitter(payload, callback);
   };
 };
-
 export { useBackend };
