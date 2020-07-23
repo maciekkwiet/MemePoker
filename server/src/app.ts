bac7107a7a4c08a4f08d23121c4e06b2265bafb0
Maciej Kwietniewski <maciek.kwiet36@gmail.com>
2020-07-23 16:26:49 +0200

    add morgan, we need?

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,8 @@ import * as dotenv from 'dotenv';
 import * as express from 'express';
 import * as mongoose from 'mongoose';
 import * as cookieParser from 'cookie-parser';
+import * as fs from 'fs';
+import * as morgan from 'morgan';
 
 import { socketRouter } from './routes/socket';
 import { router } from 'routes';
@@ -31,3 +33,17 @@ server.listen(port, () => console.log(`Server listening on port ${port}`));
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(router);
+
+if (process.env.NODE_ENV == 'production') {
+  let accessLogStream = fs.createWriteStream(__dirname + '/logs/' + 'access.log', { flags: 'a' });
+  app.use(
+    morgan('dev', {
+      skip: function (req, res) {
+        return res.statusCode < 400;
+      },
+      stream: accessLogStream,
+    })
+  );
+} else {
+  app.use(morgan('dev'));
+}
