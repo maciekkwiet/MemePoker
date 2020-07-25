8afab3da140529a478ce2678089ce13df4fd9940
KlaraGajaszek <klara.gajaszek@student.po.edu.pl>
2020-07-25 20:09:01 +0200

    fix

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -24,7 +24,10 @@ const Home = () => {
             <Typography variant="h6">CREATE NEW SESSION</Typography>
             <CreateNewRoom />
           </div>
-          <Divider orientation="vertical" flexItem className={classes.divider} />
+          <div>
+            <Divider orientation="vertical" className={classes.divider} />
+          </div>
+
           <div className={classes.wrapperItemColumn}>
             <Typography variant="h6">ADD ROOM'S NUMBER</Typography>
             <JoinRoom />
