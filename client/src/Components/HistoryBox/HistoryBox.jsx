21a72fc0a95ce32a0123b28b6ba13e12b7ea360b
wojciechowski-piotr <pedro9493@gmail.com>
2020-07-25 15:31:30 +0200

    delete test code

--- client/src/Components/HistoryBox/HistoryBox.jsx
+++ client/src/Components/HistoryBox/HistoryBox.jsx
@@ -5,14 +5,12 @@ import { useSocket } from 'socketio-hooks';
 
 import HistoryModalContent from 'Components/HistoryModalContent/';
 import HistoryElement from 'Components/HistoryElement/HistoryElement';
-import { useBackend } from 'hooks/useBackend';
 import { useRoomContext } from 'Contexts/RoomContext';
 
 const HistoryBox = () => {
   const { room } = useRoomContext();
   const [open, setOpen] = useState(false);
   const [history, setHistory] = useState(room.history);
-  const sendEstimation = useBackend('SUBMIT_ESTIMATION');
 
   const handleOpen = () => {
     setOpen(true);
@@ -22,10 +20,6 @@ const HistoryBox = () => {
     setOpen(false);
   };
 
-  const onClickHandler = () => {
-    sendEstimation();
-  };
-
   useSocket('TASK_ARCHIVED', tasks => {
     setHistory(tasks);
     console.log(tasks);
@@ -33,7 +27,6 @@ const HistoryBox = () => {
 
   return (
     <Box display="inline-block">
-      <Chip label="Submit estimation" color="primary" onClick={onClickHandler} />
       <Chip label="History" icon={<HistoryIcon />} color="primary" onClick={handleOpen} />
       <Dialog open={open} onClose={handleClose} scroll="paper" fullWidth={true} maxWidth="md">
         <HistoryModalContent onClose={handleClose} modalTitle="History">
