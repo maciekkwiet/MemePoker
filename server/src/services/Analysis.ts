c581cd87892f9f1503db3d3f4a66f8909a76948b
unknown <kaacper.janusz@gmail.com>
2020-07-27 12:32:29 +0200

    non-number vote fix

--- server/src/services/Analysis.ts
+++ server/src/services/Analysis.ts
@@ -1,12 +1,16 @@
+import { type } from 'os';
+
 class Analysis {
   average: number;
   median: number;
   standardDeviation: number;
 
-  constructor(array: number[]) {
-    this.average = array.reduce((a, b) => a + b, 0) / array.length;
-    this.median = this.countMedian(array);
-    this.standardDeviation = this.countStandardDeviation(array);
+  constructor(array: string[]) {
+    const fitered = this.checkArray(array);
+
+    this.average = this.countAverage(fitered);
+    this.median = this.countMedian(fitered);
+    this.standardDeviation = this.countStandardDeviation(fitered);
   }
   countMedian(array: number[]) {
     const mid = Math.floor(array.length / 2),
@@ -14,16 +18,28 @@ class Analysis {
     return array.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
   }
   countStandardDeviation(array: number[]) {
+    const avg = this.countAverage(array);
     const squareDiffs = array.map(function (value) {
-
-      const diff = value - array.reduce((a, b) => a + b, 0) / array.length;
+      const diff = value - avg;
       const sqrDiff = diff * diff;
       return sqrDiff;
     });
-
     const avgSquareDiff = squareDiffs.reduce((a, b) => a + b, 0) / squareDiffs.length;
     const stdDev = Math.sqrt(avgSquareDiff);
     return stdDev;
+  }
+  countAverage(array: number[]) {
+    let sum = array.reduce((sum, x) => sum + x);
+    const avg = sum / array.length;
+    return avg;
+  }
+  checkArray(array: string[]): number[] {
+    const checked = array
+      .map(i => parseInt(i, 10))
+      .filter(i => {
+        return i >= 0;
+      });
+    return checked;
   }
 }
 export { Analysis };
