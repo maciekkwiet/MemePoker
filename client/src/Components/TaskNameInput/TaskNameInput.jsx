04f091a3f09c9d3fdc337debb0ef0f228c2895bc
KlaraGajaszek <klara.gajaszek@student.po.edu.pl>
2020-08-01 00:02:42 +0200

    first version

--- client/src/Components/TaskNameInput/TaskNameInput.jsx
+++ client/src/Components/TaskNameInput/TaskNameInput.jsx
@@ -1,37 +1,49 @@
 import React from 'react';
-import { Box, Button } from '@material-ui/core';
-import SendIcon from '@material-ui/icons/Send';
-import { useUserContext } from 'Contexts/UserContext';
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
 
+import { useUserContext } from 'Contexts/UserContext';
+import { TaskNameInputStyles, CustomLabel } from './TaskNameInputStyles';
 import { useBackend } from 'hooks/useBackend';
-import { TaskNameInputStyles, CustomLabel, CustomInput } from './TaskNameInputStyles';
+
+import { Box, Button, TextField } from '@material-ui/core';
+import SendIcon from '@material-ui/icons/Send';
+
+const Schema = yup.object().shape({
+  task: yup.string().required(),
+});
 
 const TaskNameInput = () => {
   const { user } = useUserContext();
   const sendTask = useBackend('NEW_TASK');
   const classes = TaskNameInputStyles();
 
-  const onSubmitHandler = e => {
-    e.preventDefault();
-    const task = e.target.taskName.value;
+  const { register, handleSubmit, errors } = useForm({
+    validationSchema: Schema,
+  });
+
+  const onSubmitHandler = ({ task }) => {
     sendTask({ task });
-    e.target.reset();
   };
 
   if (!user.isAdmin) return null;
 
   return (
-    <form onSubmit={onSubmitHandler}>
+    <form onSubmit={handleSubmit(onSubmitHandler)}>
       <Box className={classes.root} mb={2}>
-        <CustomLabel htmlFor="taskName">Task name:</CustomLabel>
-        <CustomInput
-          id="taskName"
-          name="taskName"
-          variant="filled"
-          placeholder="TYPE A TASK"
+        <CustomLabel htmlFor="task">Task name:</CustomLabel>
+        <TextField
+          className={errors.task ? classes.textFieldError : classes.textField}
+          id="task"
+          name="task"
+          variant="outlined"
+          placeholder={errors.task ? errors.task.message : 'Task Name'}
           fullWidth
           autoComplete="off"
+          inputRef={register}
+          error={!!errors.task}
         />
+
         <Button className={classes.btn} color="primary" variant="contained" type="submit">
           <SendIcon />
         </Button>
