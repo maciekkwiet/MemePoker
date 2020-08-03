a8ab84df812e296f9c14fb00a27914ed871d4f28
Maciej Kwietniewski <maciek.kwiet36@gmail.com>
2020-08-03 20:32:26 +0200

    isObserver send to backend

--- client/src/Components/ObserverCheckbox/ObserverCheckbox.jsx
+++ client/src/Components/ObserverCheckbox/ObserverCheckbox.jsx
@@ -1,15 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 
-export default function CheckboxLabels() {
-  const [state, setState] = useState(false);
-
-  const handleChange = () => {
-    setState(!state);
-  };
-
+const CheckboxLabel = ({ checked, handleChange }) => {
   return (
-    <FormControlLabel control={<Checkbox checked={state} onChange={handleChange} color="primary" />} label="Observer" />
+    <FormControlLabel
+      control={<Checkbox checked={checked} onChange={handleChange} color="primary" />}
+      label="Observer"
+    />
   );
-}
+};
+
+export default CheckboxLabel;
