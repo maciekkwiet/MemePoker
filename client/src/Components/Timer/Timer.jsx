aef5e74d8a565e5d8c3a58197b1c918e039ff176
Tom-Bul <t.bulat99@gmail.com>
2020-08-04 21:36:40 +0200

    added better interval, padding and formating function

--- client/src/Components/Timer/Timer.jsx
+++ client/src/Components/Timer/Timer.jsx
@@ -1,44 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useSocket } from 'socketio-hooks';
 
 import InfoBox from 'Components/InfoBox';
+import useInterval from './useInterval';
 
 const Timer = () => {
   const [seconds, setSeconds] = useState(0);
-  const [minutes, setMinutes] = useState(0);
   const [isActive, setIsActive] = useState(false);
 
   const reset = () => {
     setSeconds(0);
-    setMinutes(0);
     setIsActive(false);
   };
 
-  const pad = (number, size) => {
-    var s = String(number);
-    while (s.length < (size || 2)) {
-      s = '0' + s;
-    }
-    return s;
+  const padZeroFromLeft = (time, length) => {
+    return (new Array(length + 1).join('0') + time).slice(-length);
+  };
+  const timeFormater = time => {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    return padZeroFromLeft(minutes, 2) + ':' + padZeroFromLeft(seconds, 2);
   };
 
-  useEffect(() => {
-    let interval = null;
-    if (isActive) {
-      interval = setInterval(() => {
-        if (minutes === 59 && seconds === 59) {
-          setSeconds(0);
-          setMinutes(0);
-        } else if (seconds === 59) {
-          setSeconds(0);
-          setMinutes(minutes => minutes + 1);
-        } else setSeconds(seconds => seconds + 1);
-      }, 1000);
-    } else if (!isActive && seconds !== 0) {
-      clearInterval(interval);
-    }
-    return () => clearInterval(interval);
-  }, [isActive, seconds, minutes]);
+  useInterval(() => {
+    if (isActive) setSeconds(seconds + 1);
+  }, 1000);
 
   useSocket('TASK_UPDATED', () => {
     reset();
@@ -49,7 +35,7 @@ const Timer = () => {
     setIsActive(false);
   });
 
-  return <InfoBox title="TIMER" value={pad(minutes, 2) + ':' + pad(seconds, 2)} padding={0.25} />;
+  return <InfoBox title="TIMER" value={timeFormater(seconds)} padding={0.25} />;
 };
 
 export default Timer;
