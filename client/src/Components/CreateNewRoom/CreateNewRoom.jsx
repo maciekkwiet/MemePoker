b8c7333eca8c03248e719a05604706fdb0761029
Tom-Bul <t.bulat99@gmail.com>
2020-08-10 12:30:23 +0200

    changed ErrorSocket into a dummy component

--- client/src/Components/CreateNewRoom/CreateNewRoom.jsx
+++ client/src/Components/CreateNewRoom/CreateNewRoom.jsx
@@ -9,25 +9,23 @@ import ErrorProp from 'Components/ErrorBox/ErrorProp';
 const CreateNewRoom = () => {
   const history = useHistory();
   const classes = CreateNewRoomStyles();
-  const [error, setError] = useState(undefined);
+  const [error, setError] = useState(false);
 
   const getRoomNumber = async () => {
+    setError(false);
     try {
       const currentRoom = await axios.post('/api/session');
       history.push(`/room/${currentRoom.data.room.id}/join`, { isAdmin: true });
     } catch (e) {
       console.error(e);
-      setError(e);
-      setTimeout(() => {
-        setError(undefined);
-      }, 6000);
+      setError(true);
     }
   };
   return (
     <>
       <div className={classes.buttonSession}>
         <VoteButton content={'START SESSION'} btnFunction={getRoomNumber}></VoteButton>
-        {error ? <ErrorProp content="Error: Not enough rooms" /> : null}
+        {error && <ErrorProp content="Error: Not enough rooms" isOpen={error} />}
       </div>
     </>
   );
--- client/src/Components/ErrorBox/ErrorSocket.jsx
+++ client/src/Components/ErrorBox/ErrorSocket.jsx
@@ -1,69 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { Snackbar, IconButton } from '@material-ui/core';
-import MuiAlert from '@material-ui/lab/Alert';
-import CloseIcon from '@material-ui/icons/Close';
+import React, { useState } from 'react';
 import { useSocket } from 'socketio-hooks';
 
+import ErrorProp from 'Components/ErrorBox/ErrorProp';
+
 const ErrorSocket = () => {
-  const [snackPack, setSnackPack] = useState([]);
-  const [open, setOpen] = useState(false);
-  const [error, setError] = useState(undefined);
-
-  useEffect(() => {
-    if (snackPack.length && !error) {
-      setError({ ...snackPack[0] });
-      setSnackPack(prev => prev.slice(1));
-      setOpen(true);
-    } else if (snackPack.length && error && open) {
-      setOpen(false);
-    }
-  }, [snackPack, error, open]);
-
-  const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-    setOpen(false);
-  };
-  const handleExited = () => {
-    setError(undefined);
-  };
+  const [error, setError] = useState(null);
 
   useSocket('EXCEPTION', message => {
-    setSnackPack(prev => [...prev, { message, key: new Date().getTime() }]);
+    setError(null);
+    setError(message);
   });
 
-  return (
-    <div>
-      <Snackbar
-        key={error ? error.key : undefined}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-        open={open}
-        autoHideDuration={6000}
-        onClose={handleClose}
-        onExited={handleExited}
-      >
-        <MuiAlert
-          severity="error"
-          variant="filled"
-          elevation={6}
-          onClose={handleClose}
-          action={
-            <React.Fragment>
-              <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
-                <CloseIcon fontSize="small" />
-              </IconButton>
-            </React.Fragment>
-          }
-        >
-          {error ? error.message : undefined}
-        </MuiAlert>
-      </Snackbar>
-    </div>
-  );
+  return <div>{error && <ErrorProp content={error} isOpen={error} />}</div>;
 };
 
 export default ErrorSocket;
