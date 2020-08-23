7f4d09e076567c274a74e834d074bd1c9ebe7dba
Tom-Bul <t.bulat99@gmail.com>
2020-08-23 20:37:42 +0200

    deleted comment

--- server/src/controllers/onUserVote.ts
+++ server/src/controllers/onUserVote.ts
@@ -6,7 +6,6 @@ const onUserVote: EventHandler<UserVotePayload> = ({ io }, { value, room, user }
   if (room.getUser(user.name).isObserver) return;
 
   user.vote = value;
-  // user.hasVoted = true;
 
   const userInfo = { name: user.name, hasVoted: user.hasVoted };
 
