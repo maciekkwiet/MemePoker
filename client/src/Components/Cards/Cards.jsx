c3cb26e18ea57ac4bcb954de439f26066f7a377b
KlaraGajaszek <klaragajaszek21@wp.pl>
2020-09-08 21:39:04 +0200

    card lock

--- client/src/Components/Cards/Cards.jsx
+++ client/src/Components/Cards/Cards.jsx
@@ -4,13 +4,12 @@ import { useSocket } from 'socketio-hooks';
 import CardsStyles from './CardsStyles';
 import { cardsSchema } from './cardsSchema';
 import Card from 'Components/Card';
-import { useUserContext } from 'Contexts/UserContext';
 
 const Cards = () => {
   const classes = CardsStyles();
-  const { user } = useUserContext();
 
   const [selectedCard, setSelectedCard] = useState(null);
+  const [analysis, setAnalysis] = useState(null);
 
   const selectCard = id => {
     setSelectedCard(id);
@@ -18,12 +17,17 @@ const Cards = () => {
 
   useSocket('VOTES_CLEARED', () => {
     setSelectedCard(null);
+    setAnalysis(null);
+  });
+
+  useSocket('ROOM_VOTES', ({ task }) => {
+    setAnalysis(task.analysis);
   });
 
   return (
     <div className={classes.root}>
       {cardsSchema.map(card => (
-        <Card key={card.id} {...card} selected={selectedCard === card.id} selectCard={selectCard} />
+        <Card key={card.id} {...card} selected={selectedCard === card.id} selectCard={selectCard} analysis={analysis} />
       ))}
     </div>
   );
