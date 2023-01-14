e7957bde8d55cd6701143c4ff8c14027d2546a0a
maciekkwiet <maciek.kwiet36@gmail.com>
2023-01-14 22:23:41 +0100

    remove logger prod

--- server/src/services/logger.ts
+++ server/src/services/logger.ts
@@ -3,5 +3,6 @@ const SimpleNodeLogger = require('simple-node-logger'),
     logFilePath: 'logs/mylogfile.log',
     timestampFormat: 'YYYY-MM-DD HH:mm:ss.SSS',
   },
-  log = process.env.NODE_ENV == 'production' ? SimpleNodeLogger.createSimpleLogger(opts) : console;
+  log = console;
+// log = process.env.NODE_ENV == 'production' ? SimpleNodeLogger.createSimpleLogger(opts) : console;
 export { log };
